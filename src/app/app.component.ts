e1b37c1efbdce8a34ad65a6cc1c7b717e6e75dd3
kdena04 <jonathankdena20444@gmail.com>
2025-02-05 21:37:31 -0600

    listo4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormControl, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ejemplo4';
+
+  //propiedades
+  listaTareas:string[];
+  tarea = new FormControl();
+
+  //constructor
+  constructor() {
+    this.listaTareas = [];
+    if (typeof window !== 'undefined' && window.localStorage) {
+      let datos = localStorage.getItem('tareas');
+      if (datos != null) {
+        let arregloTareas = JSON.parse(datos);
+        if (arregloTareas != null) {
+          for (let tarea of arregloTareas) {
+            this.listaTareas.push(tarea);
+          }
+        }
+      }
+    }
+  }
+
+  agregarTarea(){
+    this.listaTareas.push(this.tarea.value);
+    localStorage.setItem('tareas', JSON.stringify(this.listaTareas));
+    this.tarea.setValue('');
+  }
+
+  borrarTarea(posicion:number){
+    this.listaTareas.splice(posicion,1);
+    localStorage.clear();
+    localStorage.setItem('tareas',JSON.stringify(this.listaTareas))
+  }
+
+  borrarTareas(){
+    localStorage.clear();
+    this.listaTareas= [];
+  }
 }
